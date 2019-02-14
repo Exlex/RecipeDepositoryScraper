a4b41a2a88cc7c21549cf90e2a3851b33cce4666
Serge Crane <crane.serge@gmail.com>
2019-02-14 12:10:20 -0500

    less output

--- scrape.py
+++ scrape.py
@@ -1,48 +1,67 @@
 import requests
-from bs4 import BeautifulSoup
 from pymongo import MongoClient
+from bs4 import BeautifulSoup
 
-db_srv_url = open('settings.cfg', 'r').readline().strip()
-client = MongoClient(db_srv_url)
+def scrape():
+	page = requests.get('https://www.therecipedepository.com/')
+	soup = BeautifulSoup(page.text, 'html.parser')
+
+
+	recipe_container = soup.find(class_='small-12 medium-6 large-6 columns')
+
+	name = recipe_container.find(class_='recipe-name').string
+	if name in recipes:
+		return 0
+	recipes[name] = 0
+	print(name)
+
+	img_url = recipe_container.find(class_='recipe-image row')['src']
+	print('\n'+img_url+'\n')
+
+	ingredients = recipe_container.findAll(class_='ingredient')
+	ingredients_list = []
+	if(len(ingredients) == 0):
+		# old style of ingredients, using other method
+		ingredients_list = recipe_container.find(class_='ingredients').get_text('\n', strip=True).replace('Ingredients', '').strip().splitlines()
+	else:
+		ingredients_list = [ingredient.string for ingredient in ingredients]
+
+	for ingredient in ingredients_list:
+			print(ingredient)
+
+
+
+
+	directions = recipe_container.find(class_='directions').get_text('\n', strip=True).replace('Directions', '').strip().splitlines()
+	for instruction in directions:
+		print(instruction)
+
+
+	post_data = {
+		'name' : name,
+		'img_url' : img_url,
+		'ingredients' : ingredients_list,
+		'directions' : directions
+	}
+
+	return post_data
+
+
+
+recipes = {}
+settings = open('settings.cfg', 'r')
+client = MongoClient(settings.readline().strip())
+settings.close()
 db = client['myDb']
 
-page = requests.get('https://www.therecipedepository.com/')
-soup = BeautifulSoup(page.text, 'html.parser')
+i = 100 # how many recipes to scrape
 
+while(i > 0):
+	print('Scrapes left:' + str(i-1))
+	post_data = scrape()
+	if post_data:
+		result = db.recipes.insert_one(post_data)
 
-recipe_container = soup.find(class_='small-12 medium-6 large-6 columns')
+	
+	i -= 1
 
-name = recipe_container.find(class_='recipe-name').string
-print(name)
-
-img_url = recipe_container.find(class_='recipe-image row')['src']
-print('\n'+img_url+'\n')
-
-ingredients = recipe_container.findAll(class_='ingredient')
-ingredients_list = []
-if(len(ingredients) == 0):
-	# old style of ingredients, using other method
-	ingredients_list = recipe_container.find(class_='ingredients').get_text('\n', strip=True).replace('Ingredients', '').strip().splitlines()
-else:
-	ingredients_list = [ingredient.string for ingredient in ingredients]
-
-for ingredient in ingredients_list:
-		print(ingredient)
-
-
-
-
-directions = recipe_container.find(class_='directions').get_text('\n', strip=True).replace('Directions', '').strip().splitlines()
-for instruction in directions:
-	print(instruction)
-
-
-post_data = {
-	'name' : name,
-	'img_url' : img_url,
-	'ingredients' : ingredients_list,
-	'directions' : directions
-}
-
-result = db.recipes.insert_one(post_data)
-print(result)
